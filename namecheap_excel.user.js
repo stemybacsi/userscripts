87606c950d2cd27c986a72ac225142b5cff81454
Stemler Balázs <stemybacsi@gmail.com>
2013-05-19 16:35:15 +0200

    teszt

--- namecheap_excel.user.js
+++ namecheap_excel.user.js
@@ -4,7 +4,7 @@
 // @include      https://*manage.www.namecheap.com/*
 // @author       Stemy
 // @description  A namecheap.com webhely domain beállításait könnyebb vele módosítani
-// @version      1.0.8
+// @version      1.0.9
 // ==/UserScript==
 
 function requireFiles() 
@@ -23,6 +23,7 @@ function requireFiles()
 	
 	var libFiles = [
 		{type: "css", src: "https://raw.github.com/warpech/jquery-handsontable/master/dist/jquery.handsontable.full.css"},
+		{type: "css", src: "https://raw.github.com/stemybacsi/userscripts/master/test.css"},
 		{type: "js", src: "https://raw.github.com/stemybacsi/userscripts/master/jquery-2.0.0.min.js"},
 		{type: "js", src: "https://raw.github.com/warpech/jquery-handsontable/master/dist/jquery.handsontable.full.js"},
 		{type: "js", src: "https://raw.github.com/stemybacsi/userscripts/master/namecheap_excel_main.js"},
