8d9df48b32bf8b80033ce34ebfec518f151af664
Stemler Balázs <stemybacsi@gmail.com>
2013-07-04 23:14:20 +0200

    ghgfhfgh

--- etr_experience_main.js
+++ etr_experience_main.js
@@ -1,12 +1,14 @@
 var jq = jQuery.noConflict();
 jq(document).ready(function() {
-	jq.each($('.kurz_ul .vegleges_kurz'), function(ind, el) {
+	jq.each(jq('.kurz_ul .vegleges_kurz'), function(ind, el) {
 		var kurznev = jq(el).find('td').eq(2);
 		var kurznev_temp = kurznev.clone();
 		kurznev_temp.find('br').remove();
 		kurznev_temp.find('span').remove();
 		var kurznev_str = kurznev_temp.text().replace('\n', '').trim();
 		var kurztelj = jq(el).find('.utolso_telj');
-		console.log(kurznev_str);
+		if (kurznev_str == "Tehetséggondozás: Programozás I. ") {
+			kurztelj.html("Ko: 2");
+		}
 	});
 });
