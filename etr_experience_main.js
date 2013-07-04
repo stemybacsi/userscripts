71dc4764bd801f798f8ec3015f2bdf0965d874fc
Stemler Balázs <stemybacsi@gmail.com>
2013-07-04 23:01:33 +0200

    etr 4

--- etr_experience_main.js
+++ etr_experience_main.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 	$.each($('.kurz_ul .vegleges_kurz'), function(ind, el) {
 		var kurznev = $(el).find('td').eq(2);
+		var kurznev_temp = kurznev.clone();
+		kurznev_temp.find('br').remove();
+		kurznev_temp.find('span').remove();
 		var kurztelj = $(el).find('.utolso_telj');
-		console.log(kurznev.text());
+		console.log(kurznev_temp.text());
 	});
 });
