64c6a3c7b648826ee1d43e23377ce67ecb39e8dd
Stemler Balázs <stemybacsi@gmail.com>
2013-07-04 23:10:22 +0200

    fds

--- etr_experience_main.js
+++ etr_experience_main.js
@@ -1,11 +1,12 @@
-$(document).ready(function() {
-	$.each($('.kurz_ul .vegleges_kurz'), function(ind, el) {
-		var kurznev = $(el).find('td').eq(2);
+var jq = jQuery.noConflict();
+jq(document).ready(function() {
+	jq.each($('.kurz_ul .vegleges_kurz'), function(ind, el) {
+		var kurznev = jq(el).find('td').eq(2);
 		var kurznev_temp = kurznev.clone();
 		kurznev_temp.find('br').remove();
 		kurznev_temp.find('span').remove();
 		var kurznev_str = kurznev_temp.text().replace('\n', '').trim();
-		var kurztelj = $(el).find('.utolso_telj');
+		var kurztelj = jq(el).find('.utolso_telj');
 		console.log(kurznev_str);
 	});
 });
