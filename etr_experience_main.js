17878223b75765011b2abea4966fb0a49c9d4efd
Stemler Balázs <stemybacsi@gmail.com>
2013-07-06 19:42:41 +0200

    etr work

--- etr_experience_main.js
+++ etr_experience_main.js
@@ -2,30 +2,17 @@ var urlArray = window.location.href.split('/etr/');
 //Melyik oldalon vagyunk
 if (urlArray[1] == "VizsgaHallg") {
 	//Vizsgajelentkezés
-	//var jq = jQuery.noConflict();
-	console.log($('ul').length);
-	/*$.each(jq('.hvl_kurzus_lista .kurzus_div'), function(ind, el) {
-		var kurznev = jq(el).find('.kurzus_div_head b');
-		var kurznev_str = kurznev.text();
-		var kurztelj = jq(el).find('.meglevo_jegy').last();
-		
-		if (kurznev_str == "Tehetséggondozás: Programozás I.") {
-			kurztelj.html(" Ko: <span>elégséges (2)</span>");
-		} else {
-			console.log(kurznev_str);
-		}
-	});*/
+	vizsgaHallg();
 } else {
 	//Kurzuslista
-	var jq = jQuery.noConflict();
-	jq(document).ready(function() {
-		jq.each(jq('.kurz_ul .vegleges_kurz'), function(ind, el) {
-			var kurznev = jq(el).find('td').eq(2);
+	$(document).ready(function() {
+		$.each($('.kurz_ul .vegleges_kurz'), function(ind, el) {
+			var kurznev = $(el).find('td').eq(2);
 			var kurznev_temp = kurznev.clone();
 			kurznev_temp.find('br').remove();
 			kurznev_temp.find('span').remove();
 			var kurznev_str = kurznev_temp.text().replace('\n', '').trim();
-			var kurztelj = jq(el).find('.utolso_telj');
+			var kurztelj = $(el).find('.utolso_telj');
 			if (kurznev_str == "Tehetséggondozás: Programozás I.") {
 				kurztelj.html("Ko: 2");
 			} else {
@@ -33,4 +20,23 @@ if (urlArray[1] == "VizsgaHallg") {
 			}
 		});
 	});
+}
+
+function vizsgaHallg() {
+	var kurzus_lista = $('.hvl_kurzus_lista .kurzus_div');
+	if (kurzus_lista.length == 0) {
+		setTimeout(vizsgaHallg, 50);
+	} else {
+		$.each(kurzus_lista, function(ind, el) {
+			var kurznev = $(el).find('.kurzus_div_head b');
+			var kurznev_str = kurznev.text();
+			var kurztelj = $(el).find('.meglevo_jegy').last();
+			
+			if (kurznev_str == "Tehetséggondozás: Programozás I.") {
+				kurztelj.html(" Ko: <span>elégséges (2)</span>");
+			} else {
+				console.log(kurznev_str);
+			}
+		});
+	}
 }
