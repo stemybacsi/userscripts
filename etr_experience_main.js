cf6c801097fdd9e26cddcba7286edb7d08473e8b
Stemler Balázs <stemybacsi@gmail.com>
2013-07-06 19:32:17 +0200

    etr new 3

--- etr_experience_main.js
+++ etr_experience_main.js
@@ -1,23 +1,8 @@
-var jq = jQuery.noConflict();
 var urlArray = window.location.href.split('/etr/');
-alert(urlArray[1]);
-jq(document).ready(function() {
-	//Kurzuslista
-	jq.each(jq('.kurz_ul .vegleges_kurz'), function(ind, el) {
-		var kurznev = jq(el).find('td').eq(2);
-		var kurznev_temp = kurznev.clone();
-		kurznev_temp.find('br').remove();
-		kurznev_temp.find('span').remove();
-		var kurznev_str = kurznev_temp.text().replace('\n', '').trim();
-		var kurztelj = jq(el).find('.utolso_telj');
-		if (kurznev_str == "Tehetséggondozás: Programozás I.") {
-			kurztelj.html("Ko: 2");
-		} else {
-			console.log(kurznev_str);
-		}
-	});
+//Melyik oldalon vagyunk
+if (urlArray[1] == "VizsgaHallg") {
 	//Vizsgajelentkezés
-	jq.each(jq('.hvl_kurzus_lista .kurzus_div'), function(ind, el) {
+	/*jq.each(jq('.hvl_kurzus_lista .kurzus_div'), function(ind, el) {
 		var kurznev = jq(el).find('.kurzus_div_head b');
 		var kurznev_str = kurznev.text();
 		var kurztelj = jq(el).find('.meglevo_jegy').last();
@@ -27,5 +12,24 @@ jq(document).ready(function() {
 		} else {
 			console.log(kurznev_str);
 		}
+	});*/
+	alert("VMI");
+} else {
+	//Kurzuslista
+	var jq = jQuery.noConflict();
+	jq(document).ready(function() {
+		jq.each(jq('.kurz_ul .vegleges_kurz'), function(ind, el) {
+			var kurznev = jq(el).find('td').eq(2);
+			var kurznev_temp = kurznev.clone();
+			kurznev_temp.find('br').remove();
+			kurznev_temp.find('span').remove();
+			var kurznev_str = kurznev_temp.text().replace('\n', '').trim();
+			var kurztelj = jq(el).find('.utolso_telj');
+			if (kurznev_str == "Tehetséggondozás: Programozás I.") {
+				kurztelj.html("Ko: 2");
+			} else {
+				console.log(kurznev_str);
+			}
+		});
 	});
-});
+}
